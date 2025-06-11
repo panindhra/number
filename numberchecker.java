9af5ca3f405e2905e9c5030ecd54ad105e9c7149
panindhra <phanindhracharigudipati@gmail.com>
2025-06-11 06:12:30 +0530

    save the file

---
+++ numberchecker.java
@@ -0,0 +1,18 @@
+public class numberchecker{
+    public static void main (String [] args){
+        int num = -5;
+        if (num > 0){
+            System.out.println("number is positive");
+        }
+        else if (num < 0){
+            System.out.println("number is negative");
+        }
+         
+            
+         else {
+            
+        }{
+            System.out.println("zero");
+        }
+    }
+}
